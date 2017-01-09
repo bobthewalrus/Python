fa3bda464f66f7144588ed6db3fb6eac71bc0c41
James Chantler <chantler.james@gmail.com>
2017-01-09 15:13:04 -0800

    Added Oddeven

---
+++ Oddeven/Oddeven.py
@@ -0,0 +1,5 @@
+for count in range(1,2001):
+    if count % 2 ==1:
+        print "Number is {}. This is an odd number.".format(count)
+    if count % 2 ==0:
+        print "Number is {}. This is an even number.".format(count)
